575939551a132a9a6c762f7b08ccffdb72aabefe
FadeevEgor <FadeevEgor@users.noreply.github.com>
2022-06-19 19:57:27 +0000

    deploy: 9e74c192e76724fb5c8b306078f33026834f9aeb

---
+++ scripts/numpy/loops_numpy.py
@@ -0,0 +1,16 @@
+from time import perf_counter
+from numpy.random import uniform
+
+
+N = 10_000_000
+
+# Создаём массивы случайных чисел
+a = uniform(-1, -1, size=N)
+b = uniform(-1, -1, size=N)
+
+
+# Измеряем время сложения этих списков
+t1 = perf_counter()
+c = a + b        
+t2 = perf_counter()
+print(f"{(t2 - t1) * 1000} ms") # perf_counter() - время в секундах
