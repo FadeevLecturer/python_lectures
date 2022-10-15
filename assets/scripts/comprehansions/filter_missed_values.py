f24f5f28e56fd72630368e7d203f2ee5511a01c2
Fadeev Egor <fadeevegor@yandex.ru>
2022-10-15 14:01:16 +0300

    first homework

---
+++ assets/scripts/comprehansions/filter_missed_values.py
@@ -0,0 +1,21 @@
+import unittest
+from math import nan, isnan, inf
+
+
+class FilterMissingValuesTest(unittest.TestCase):
+    def test_1(self):
+        x = [None, 3.14, None, None, 2.71, 1.41, None]
+        y = [3.14, 2.71, 1.41]
+        self.assertEqual(filter_missed_values(x), y)
+        
+    def test_2(self):
+        x = [inf/inf, 3.14, nan, float("nan"), 2.71, 1.41, inf - inf]
+        y = [3.14, 2.71, 1.41]
+        self.assertEqual(filter_missed_values(x), y)
+
+
+def filter_missed_values(data):
+    pass
+    
+if __name__ == "__main__":
+    unittest.main()
