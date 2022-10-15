9dbb07a77fe53fec890ae0fcf488d88a6f6c75f9
FadeevEgor <FadeevEgor@users.noreply.github.com>
2022-10-15 11:05:16 +0000

    deploy: f24f5f28e56fd72630368e7d203f2ee5511a01c2

---
+++ scripts/comprehansions/means.py
@@ -0,0 +1,43 @@
+import unittest
+from math import prod, pow 
+
+
+class ArithmeticMeanTest(unittest.TestCase):
+    def test_1(self):
+        x = [1, 2, 3]
+        self.assertAlmostEqual(arithmetic_mean(x), 2)
+        
+    def test_2(self):
+        self.assertIsNone(arithmetic_mean([]), None)
+
+
+class GeometricMeanTest(unittest.TestCase):
+    def test_1(self):
+        x = [1, 2, 3]
+        self.assertAlmostEqual(geometric_mean(x), pow(6, 1/3))
+        
+    def test_2(self):
+        self.assertIsNone(geometric_mean([]), None)
+
+
+class HarmonicMeanTest(unittest.TestCase):
+    def test_1(self):
+        x = [1, 2, 3]
+        self.assertAlmostEqual(harmonic_mean(x), 18/11)
+        
+    def test_2(self):
+        self.assertIsNone(harmonic_mean([]), None)
+
+
+def arithmetic_mean(data):
+    pass
+    
+def geometric_mean(data):
+    pass
+    
+def harmonic_mean(data):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
