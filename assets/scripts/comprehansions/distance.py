4720e2c606570e061f1e712db702de5c7581d4e3
Egor Fadeev <fadeevegor@yandex.ru>
2022-11-21 21:56:16 +0300

    Немного обновил списковые включения

--- assets/scripts/comprehansions/distance.py
+++ assets/scripts/comprehansions/distance.py
@@ -1,16 +1,7 @@
 import unittest
-from numbers import Number
+
 
 class DistanceTest(unittest.TestCase):
-    def test_1(self):
-        self.assertEqual(distance(42, 13), 29)
-        
-    def test_2(self):
-        self.assertAlmostEqual(distance(3.14, 2.71), 0.43000000000000016)
-
-    def test_3(self):
-        self.assertAlmostEqual(distance(1+2j, 3+4j), 2.8284271247461903)
-
     def test_4(self):
         self.assertEqual(distance("abc", "cba"), 2)
 
@@ -18,11 +9,9 @@ class DistanceTest(unittest.TestCase):
         self.assertEqual(distance("Течение", "Течении"), 1)
 
 
-        
-
-
-def distance(x, y):
+def distance(x: str, y: str):
     pass
+
 
 if __name__ == "__main__":
     unittest.main()
